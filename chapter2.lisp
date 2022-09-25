362fc64b655e435ea9d8c75dbf91368b2c13ce03
iamalanxue <alanxue43@gmail.com>
2022-09-25 11:56:11 -0500

    almost done with chapter 2

--- chapter2.lisp
+++ chapter2.lisp
@@ -10,10 +10,34 @@
         ((null lst) nil)
         ((listp (car lst)) t)
         (t (has-list-p (cdr lst)))))
-        
+
 (defun print-dots (x)
     (cond
         ((= 0 x) nil)
         (t
             (format t ".")
             (print-dots (1- x)))))
+
+(defun print-dots (x)
+    (do ((i 0 (1+ i)))
+        ((= i x) nil)
+        (format t ".")))
+
+(defun get-a-count (lst)
+    (cond 
+        ((null lst) 0)
+        (t (+ (if (= 'a (car lst)) 1 0)
+            (get-a-count (cdr lst))))))
+
+(defun get-a-count (lst)
+    (do ((ll lst (cdr ll))
+        (n 0 (if (= 'a (car lst)))))))
+
+;; he wants to remove all the nil in the list but the function `remove` leaves the original list untouched
+
+(defun summit (lst)
+    (setf lst (remove nil lst))
+    (apply #'+ lst))
+
+(defun summit (lst)
+    )
